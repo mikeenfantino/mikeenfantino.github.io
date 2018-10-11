1c6cf83c81a6def1ddc80f571f5431d412d52d6b
Mike Enfantino <mikeenfantino@gmail.com>
2018-10-11 16:01:50 -0700

    Revamped content

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
-$(document).ready(function() {
+jQuery(function($) { // jQuery no conflict
 
   // Magnific Popup
   $('.popup-with-zoom-anim').magnificPopup({
-		type: 'inline',
-		fixedContentPos: false,
-		fixedBgPos: true,
-		overflowY: 'auto',
-		closeBtnInside: true,
-		preloader: false,
-		midClick: true,
-		removalDelay: 300,
-		mainClass: 'my-mfp-zoom-in'
-	});
+    type: 'inline',
+    fixedContentPos: false,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+  });
 
-	// MatchHeight
-	$('.js-matchheight-box').matchHeight();
-	$('.js-matchheight-box-header').matchHeight();
+  // MatchHeight
+  $('.js-matchheight-box').matchHeight();
+  $('.js-matchheight-box-header').matchHeight();
 
   // Back to Top
   $(".js-back-to-top").on("click", function() {
     $("html, body").animate({
-        scrollTop: 0
+      scrollTop: 0
     }, 800);
   });
 
